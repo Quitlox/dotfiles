7eae2c4852b0c8457f485e74da01ce5ce6b27494
Kevin Witlox <kevin.witlox@upcmail.nl>
2022-02-03 13:10:52 +0100

    [polybar] add debug messages to new_multi_launch.sh

--- private_dot_config/polybar/executable_new_multi_launch.sh
+++ private_dot_config/polybar/executable_new_multi_launch.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# This script reloads all polybar instances for bspwm.
+source "$HOME/.config/dotfiles/include/dep_colors.sh"
+
+# This script reloads all polybar instances for i3.
 # Stolen from: https://github.com/polybar/polybar/issues/763#issuecomment-726872004
+debug "Running new_multi_launch.sh"
 
 # Fail if DISPLAY is not set
 if [ -z "${DISPLAY}" ] ; then
@@ -19,7 +22,9 @@ if [ -n "${XDG_CACHE_HOME}" ] ; then
     echo "Lock directory location is not a directory!"
     exit 2
   fi
+  echo "${XDG_CACHE_HOME}/polybar/bars.at.${DISPLAY}.lock"
 else
+  echo "Lock file: /tmp/polybars.at.${DISPLAY}.lock"
   lock="/tmp/polybars.at.${DISPLAY}.lock"
 fi
 
@@ -32,13 +37,16 @@ if [ -n "${XDG_DATA_HOME}" ] ; then
     echo "Log directory location is not a directory!"
     exit 2
   fi
+  echo "${XDG_DATA_HOME}/polybar"
 else
+  echo "Log file: /tmp"
   logd="/tmp"
 fi
 
 # Run this code block
 ( flock 200
   #---Kill all running polybars on this display
+  echo "Killing running polybar instances. (running: $(pgrep 'polybar' | xargs))"
   pgrep 'polybar' | while IFS= read -r _pid ; do
     if [ "${_pid}" != "$$" ] ; then
       # Check the DISPLAY variable of this instance is the same with this one
@@ -55,18 +63,20 @@ fi
       done
     fi
   done
+  echo "Killed polybar instances. (running: $(pgrep 'polybar'))"
 
   #---Get name of primary monitor
   _primon="$(polybar --list-monitors | awk '/primary/ {print substr($1, 1, length($1)-1)}')"
   if [ -z "${_primon}" ] ; then
     _primon="$(polybar --list-monitors | head -n 1 | awk '{print substr($1, 1, length($1)-1)}')"
   fi
+  debug "Primary monitor set to $_primon"
 
   #---Launch polybar on all monitors
   polybar --list-monitors | while IFS= read -r _pom ; do
     #--Format log location
     _mon="$(awk -F '[ ,:]' '{print $1}' <<< "${_pom}")"
-    _log="${logd}/log-bspwm-${DISPLAY}-${_mon}"
+    _log="${logd}/log-i3-${DISPLAY}-${_mon}"
     if [ -e "${_log}" ] ; then
       rm --recursive --force "${_log}"
     fi
@@ -82,13 +92,15 @@ fi
 
     # Launch the bars
     if [ "${_mon}" = "${_primon}" ]; then
+      debug "Launching polybars for $_mon (primary)..."
       # Launch the main bars
-      polybar --reload top </dev/null >"${_log}-t" 2>&1 & disown
-      polybar --reload bottom </dev/null >"${_log}-b" 2>&1 & disown
+      nohup polybar --reload top </dev/null 2>&1 | tee "${_log}-t" & disown
+      nohup polybar --reload bottom </dev/null 2>&1 | tee "${_log}-b" & disown
     else
+      debug "Launching polybars for $_mon..."
       # Launch an auxillary bar
-      polybar --reload top </dev/null >"${_log}-t" 2>&1 & disown
-      polybar --reload bottom </dev/null >"${_log}-b" 2>&1 & disown
+      nohup polybar --reload top </dev/null 2>&1 | tee "${_log}-t" & disown
+      nohup polybar --reload bottom </dev/null 2>&1 | tee "${_log}-b" & disown
     fi
   done
 
