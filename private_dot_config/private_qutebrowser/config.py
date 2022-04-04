090fec81039e02e83e277c6495b7c30270046a8d
Kevin Witlox <kevin.witlox@upcmail.nl>
2022-04-04 15:58:19 +0200

    [qute] add qutebrowser config

---
+++ private_dot_config/private_qutebrowser/config.py
@@ -0,0 +1,11 @@
+# pylint: disable=C0111
+c = c  # noqa: F821 pylint: disable=E0602,C0103
+config = config  # noqa: F821 pylint: disable=E0602,C0103
+# pylint: disable=C0111
+from qutebrowser.config.configfiles import ConfigAPI  # noqa: F401
+from qutebrowser.config.config import ConfigContainer  # noqa: F401
+config: ConfigAPI = config  # noqa: F821 pylint: disable=E0602,C0103
+c: ConfigContainer = c  # noqa: F821 pylint: disable=E0602,C0103
+
+config.unbind('t', mode='normal')
+config.bind('t', 'set-cmd-text -s :open -t')
