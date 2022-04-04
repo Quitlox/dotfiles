a7b560b64ea6ca2ee9d058401269c751890f0922
Kevin Witlox <kevin.witlox@upcmail.nl>
2022-04-04 18:27:37 +0200

    [qute] fix config (add autoload)

--- private_dot_config/private_qutebrowser/config.py
+++ private_dot_config/private_qutebrowser/config.py
@@ -7,6 +7,7 @@ from qutebrowser.config.config import ConfigContainer  # noqa: F401
 config: ConfigAPI = config  # noqa: F821 pylint: disable=E0602,C0103
 c: ConfigContainer = c  # noqa: F821 pylint: disable=E0602,C0103
 
-config.unbind('t', mode='normal')
+config.load_autoconfig()
+
 config.bind('t', 'set-cmd-text -s :open -t')
 config.bind('<Space>l', ':spawn --userscript qute-bitwarden')
