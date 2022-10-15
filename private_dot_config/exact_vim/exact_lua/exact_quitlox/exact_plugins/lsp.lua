4b490d2a7e26ff02119f980f6bd4d4916c9b0fd7
Kevin Witlox <kevin.witlox@upcmail.nl>
2022-10-15 20:09:27 +0200

    [nvim] update to neovim 0.8

--- private_dot_config/exact_vim/exact_lua/exact_quitlox/exact_plugins/lsp.lua
+++ private_dot_config/exact_vim/exact_lua/exact_quitlox/exact_plugins/lsp.lua
@@ -112,11 +112,11 @@ local on_attach = function(client, bufnr)
 	-- Server specific workarounds
 	if client.name == "sumneko_lua" then
 		-- We ignore the built-in formatter of sumneko_lua, and use stylua provided via null-ls
-		client.resolved_capabilities.document_formatting = false
+		client.server_capabilities.document_formatting = false
 	end
 	if client.name == "sumneko_lua" then
 		-- We ignore the built-in formatter of sumneko_lua, and use stylua provided via null-ls
-		client.resolved_capabilities.document_formatting = false
+		client.server_capabilities.document_formatting = false
 	end
 
 	-- Cursor Highlight
@@ -138,7 +138,7 @@ local on_attach = function(client, bufnr)
 end
 
 -- Completion Capabilities
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 -- capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 ----------------------------------------
