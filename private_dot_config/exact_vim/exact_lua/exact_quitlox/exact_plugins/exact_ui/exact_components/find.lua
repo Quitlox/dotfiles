025a2624cfa339c8de06d62d7f661de886aab23a
Kevin Witlox <kevin.witlox@tno.nl>
2023-02-13 13:42:38 +0100

    [nvim] fix smart_open on windows

--- private_dot_config/exact_vim/exact_lua/exact_quitlox/exact_plugins/exact_ui/exact_components/find.lua
+++ private_dot_config/exact_vim/exact_lua/exact_quitlox/exact_plugins/exact_ui/exact_components/find.lua
@@ -2,13 +2,15 @@
 -- Setup
 ----------------------------------------
 
+vim.g.sqlite_clib_path = vim.fn.expand("$HOME/.config/vim/sqlite3.dll")
+
 -- TODO: Make FZF ignore ignored files
 return {
     {
         "nvim-telescope/telescope.nvim",
         dependencies = {
             "nvim-lua/plenary.nvim",
-            { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+            -- { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
         },
         cmd = "Telescope",
         config = function()
@@ -27,19 +29,12 @@ return {
                     },
                 },
                 extensions = {
-                    fzf = {
-                        fuzzy = true, -- false will only do exact matching
-                        override_generic_sorter = true, -- override the generic sorter
-                        override_file_sorter = true, -- override the file sorter
-                        case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-                        -- the default case_mode is "smart_case"
-                    },
                     smart_open = {
                         cwd_only = true,
                     },
                 },
             })
-            require("telescope").load_extension("fzf")
+            -- require("telescope").load_extension("fzf")
         end,
 
         init = function()
