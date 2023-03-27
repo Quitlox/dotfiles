725adc9a9c8a379c8a4302e241236074278c637e
Kevin Witlox <kevin.witlox@upcmail.nl>
2023-03-27 08:00:14 +0200

    [nvim] replace comment frame with comment box plugin

--- private_dot_config/exact_vim/exact_lua/exact_quitlox/exact_plugins/exact_comment/header.lua
+++ private_dot_config/exact_vim/exact_lua/exact_quitlox/exact_plugins/exact_comment/header.lua
@@ -1,18 +1,46 @@
 -- Insert Comment Frame/Header
+-- return {
+--     "s1n7ax/nvim-comment-frame",
+--     dependencies = { "nvim-treesitter/nvim-treesitter" },
+--     keys = { "gch", "gcH" },
+--     config = true,
+--     init=function()
+--         require("which-key").register({
+--             g = {
+--                 c = {
+--                     -- TODO: Re-enable. Conflicts with Comment.nvim--[[ ? ]]
+--                     h = { "<cmd>lua require('nvim-comment-frame').add_comment()<cr>", "Comment Header" },
+--                     H = { "<cmd>lua require('nvim-comment-frame')()<cr>", "Comment Header (multi-line)" },
+--                 },
+--             },
+--         })
+--     end
+-- }
+
 return {
-    "s1n7ax/nvim-comment-frame",
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
-    keys = { "gch", "gcH" },
+    "LudoPinelli/comment-box.nvim",
     config = true,
-    init=function()
-        require("which-key").register({
+    init = function()
+        require("quitlox.util.which_key").register({
             g = {
-                c = {
-                    -- TODO: Re-enable. Conflicts with Comment.nvim--[[ ? ]]
-                    h = { "<cmd>lua require('nvim-comment-frame').add_comment()<cr>", "Comment Header" },
-                    H = { "<cmd>lua require('nvim-comment-frame')()<cr>", "Comment Header (multi-line)" },
+                b = {
+                    b = { "<cmd>lua require('comment-box').llbox(10)<cr>", "Comment Box" },
+                    c = { "<cmd>lua require('comment-box').accbox(10)<cr>", "Comment Box Centered" },
+                    l = { "<cmd>lua require('comment-box').lcline(10)<cr>", "Comment Box Line" },
+                    m = { "<cmd>lua require('comment-box').catalog()<cr>", "Comment Box Catalog" },
                 },
             },
-        })
+        }, {mode = "n"})
+         
+        require("quitlox.util.which_key").register({
+            g = {
+                b = {
+                    b = { "<cmd>lua require('comment-box').llbox(10)<cr>", "Comment Box" },
+                    c = { "<cmd>lua require('comment-box').accbox(10)<cr>", "Comment Box Centered" },
+                    l = { "<cmd>lua require('comment-box').lcline(10)<cr>", "Comment Box Line" },
+                    m = { "<cmd>lua require('comment-box').catalog()<cr>", "Comment Box Catalog" },
+                },
+            },
+        }, {mode = "v"})
     end
 }
