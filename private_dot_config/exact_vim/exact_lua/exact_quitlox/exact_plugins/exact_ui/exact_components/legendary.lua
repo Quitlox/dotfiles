f411fcad5d367c21e7cd6010a841807b8fa3444c
Kevin Witlox <kevin.witlox@tno.nl>
2023-05-01 10:58:48 +0200

    [nvim] add command for deleting shada file

--- private_dot_config/exact_vim/exact_lua/exact_quitlox/exact_plugins/exact_ui/exact_components/legendary.lua
+++ private_dot_config/exact_vim/exact_lua/exact_quitlox/exact_plugins/exact_ui/exact_components/legendary.lua
@@ -6,12 +6,35 @@ return {
     config = function()
         require("legendary").setup({
             commands = { {
+                    
                     ':DismissNotifications',
                     function()
                         require('notify').dismiss({pending = true, silent=true})
                     end,
                     description = 'Dismiss all notifications'
-                }, },
+                },
+                {
+                        ':DeleteShadaFile',
+                        function()
+                                  local shada_file = vim.fn.stdpath('data') .. '/shada/main.shada'
+  local file_exists = io.open(shada_file, "r")
+
+  if file_exists then
+    file_exists:close()
+
+    local success, err = os.remove(shada_file)
+    if success then
+      print("Shada file deleted successfully.")
+    else
+      print("Error deleting shada file: " .. tostring(err))
+    end
+  else
+    print("Shada file not found.")
+  end
+                        end,
+                        description = 'Delete the Shada file, in case of corruptions'
+                }
+        },
             which_key = {
                 auto_register = false,
                 do_binding = false,
