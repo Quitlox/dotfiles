21ea68303111c2a42c65205d482651dd208c0619
Kevin Witlox <kevin.witlox@upcmail.nl>
2023-05-01 21:43:43 +0200

    [nvim] add ]n navigation for todo notes

--- private_dot_config/exact_vim/exact_lua/exact_quitlox/exact_plugins/exact_editor/todo.lua
+++ private_dot_config/exact_vim/exact_lua/exact_quitlox/exact_plugins/exact_editor/todo.lua
@@ -19,5 +19,18 @@ return {
                 n = { "<cmd>TodoTrouble<cr>", "Open Notes" },
             },
         }, { prefix = "<leader>o", noremap = true })
+
+        local wk = require("which-key")
+
+        wk.register({
+            ["]n"] = {
+                function() require("todo-comments").jump_next() end,
+                "Next note",
+            },
+            ["[n"] = {
+                function() require("todo-comments").jump_prev() end,
+                "Previous note",
+            },
+        }, { mode = "n" })
     end,
 }
