454d7e060fa8061ade254b3180c960aeea73fbd1
Kevin Witlox <kevin.witlox@tno.nl>
2023-05-02 09:37:23 +0200

    [nvim] add gitignore.nvim plugin

--- private_dot_config/exact_nvim/exact_lua/exact_quitlox/exact_plugins/exact_ui/exact_components/legendary.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_quitlox/exact_plugins/exact_ui/exact_components/legendary.lua
@@ -1,33 +1,44 @@
-
-local function DismissNotifications()
-    require('notify').dismiss({pending = true, silent=true})
-end
+local function dismissNotifications() require("notify").dismiss({ pending = true, silent = true }) end
 
 return {
-    "mrjones2014/legendary.nvim",
-    version = "",
-    dependencies = { "kkharji/sqlite.lua" },
-    config = function()
-        require("legendary").setup({
-            commands = { {
-                    ':DismissNotifications',
-                    DismissNotifications,
-                    description = 'Dismiss all notifications'
-                }, },
-            which_key = {
-                auto_register = true,
-                do_binding = false,
-            },
-        })
+    {
+        "mrjones2014/legendary.nvim",
+        version = "",
+        dependencies = { "kkharji/sqlite.lua" },
+        config = function()
+            require("legendary").setup({
+                commands = {
+                    {
+                        ":DismissNotifications",
+                        dismissNotifications,
+                        description = "Dismiss all notifications",
+                    },
+                    {
+                        ":Gitignore",
+                        description = "Create .gitignore file",
+                    },
+                },
+                which_key = {
+                    auto_register = true,
+                    do_binding = false,
+                },
+            })
 
-        require("which-key").register({
-            k = { "<cmd>Legendary<cr>", "Keymap" },
-            d = {"<cmd>DismissNotifications<cr>", "Dismiss Notifications"},
-        }, { prefix = "<leader>v" })
-    end,
-    extensions = {
-        nvim_tree=true,
-        smart_splits=true,
-        diffview=true,
-    }
+            require("which-key").register({
+                k = { "<cmd>Legendary<cr>", "Keymap" },
+                d = { "<cmd>DismissNotifications<cr>", "Dismiss Notifications" },
+            }, { prefix = "<leader>v" })
+        end,
+        extensions = {
+            nvim_tree = true,
+            smart_splits = true,
+            diffview = true,
+        },
+    },
+    {
+        "wintermute-cell/gitignore.nvim",
+        dependencies = { "nvim-telescope/telescope.nvim" },
+        cmd = "Gitignore",
+        lazy = true,
+    },
 }
