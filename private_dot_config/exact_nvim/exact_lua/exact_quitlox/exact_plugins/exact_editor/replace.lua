08556759965fb4409b044649118b789f081d8cab
Kevin Witlox <kevin.witlox@tno.nl>
2023-05-04 08:32:21 +0200

    [nvim] add commands for replace (spectre)

--- private_dot_config/exact_nvim/exact_lua/exact_quitlox/exact_plugins/exact_editor/replace.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_quitlox/exact_plugins/exact_editor/replace.lua
@@ -1,17 +1,37 @@
 return {
     "nvim-pack/nvim-spectre",
     init = function()
-        require('which-key').register({
-            R = {"<cmd>lua require('spectre').open()<CR>", "Replace"},
+        require("which-key").register({
+            R = { "<cmd>lua require('spectre').open()<CR>", "Replace" },
             r = {
-                name="Replace",
-                w = {"<cmd>lua require('spectre').open_visual({select_word=true})<CR>", "Replace Word"},
-                f = {"<cmd>lua require('spectre').open_file_search()<CR>", "Replace in File"},
-            }
-        },{prefix="<leader>"})
+                name = "Replace",
+                w = { "<cmd>lua require('spectre').open_visual({select_word=true})<CR>", "Replace Word" },
+                f = { "<cmd>lua require('spectre').open_file_search()<CR>", "Replace in File" },
+            },
+        }, { prefix = "<leader>" })
+        require("which-key").register({
+            R = { "<cmd>lua require('spectre').open_visual()<CR>", "Replace Visual" },
+        }, { prefix = "<leader>", mode = "v" })
 
-        require('which-key').register({
-                R = {"<cmd>lua require('spectre').open_visual()<CR>", "Replace Visual"},
-        }, {prefix="<leader>", mode="v"})
-    end
+        require("legendary").command({
+            ":Replace",
+            "<cmd>lua require('spectre').open()<CR>",
+            description = "Replace word under cursor in project",
+        })
+        require("legendary").command({
+            ":ReplaceWord",
+            "<cmd>lua require('spectre').open_visual({select_word=true})<CR>",
+            description = "Replace word under cursor in project",
+        })
+        require("legendary").command({
+            ":ReplaceFile",
+            "<cmd>lua require('spectre').open_file_search()<CR>",
+            description = "Replace in file",
+        })
+        require("legendary").command({
+            ":ReplaceVisual",
+            "<cmd>lua require('spectre').open_visual()<CR>",
+            description = "Replace visual selection",
+        })
+    end,
 }
