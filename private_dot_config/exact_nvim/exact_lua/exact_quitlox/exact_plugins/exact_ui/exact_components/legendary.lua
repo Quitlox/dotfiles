1a139c787140214b3d9faa2aac0252bd6bbd1ac5
Kevin Witlox <kevin.witlox@tno.nl>
2023-05-04 11:10:51 +0200

    [nvim] legendary: add functions for removing log files

--- private_dot_config/exact_nvim/exact_lua/exact_quitlox/exact_plugins/exact_ui/exact_components/legendary.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_quitlox/exact_plugins/exact_ui/exact_components/legendary.lua
@@ -1,5 +1,19 @@
 local function dismissNotifications() require("notify").dismiss({ pending = true, silent = true }) end
 
+function deleteCacheFile(path)
+    local cache_dir = vim.fn.stdpath("cache")
+    local file_path = cache_dir .. "/" .. path
+
+    local success, err = os.remove(file_path)
+    local notify = require("notify")
+
+    if success then
+        notify("Removed " .. path .. " successfully.", "info", {})
+    else
+        notify("Error removing " .. path .. ": " .. tostring(err), "error", {})
+    end
+end
+
 return {
     {
         "mrjones2014/legendary.nvim",
@@ -7,6 +21,16 @@ return {
         dependencies = { "kkharji/sqlite.lua" },
         config = function()
             require("legendary").setup({
+                funcs = {
+                    {
+                        function() deleteCacheFile("null-ls.log") end,
+                        description = "Delete the NullLS Log file",
+                    },
+                    {
+                        function() deleteCacheFile("dap.log") end,
+                        description = "Delete the DAP Log file",
+                    },
+                },
                 commands = {
                     {
                         ":DismissNotifications",
