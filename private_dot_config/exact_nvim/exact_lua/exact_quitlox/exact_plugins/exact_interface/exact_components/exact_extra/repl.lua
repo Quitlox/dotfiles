938841267bc616c965df9b1fe44d08cdf5fed675
Kevin Witlox <kevin.witlox@upcmail.nl>
2023-06-19 11:12:38 +0200

    [nvim] configure conjure

--- private_dot_config/exact_nvim/exact_lua/exact_quitlox/exact_plugins/exact_interface/exact_components/exact_extra/repl.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_quitlox/exact_plugins/exact_interface/exact_components/exact_extra/repl.lua
@@ -1,65 +1,65 @@
 return {
-    "Olical/conjure",
-    version = "",
-    keys = "<localleader>r",
-    dependencies = {
-        {
-            "PaterJason/cmp-conjure",
-            config = function()
-                local cmp = require("cmp")
-                local config = cmp.get_config()
-                table.insert(config.sources, {
-                    name = "buffer",
-                    option = {
-                        sources = {
-                            { name = "conjure" },
-                        },
-                    },
-                })
-                cmp.setup(config)
-            end,
+    {
+        "folke/which-key.nvim",
+        optional = true,
+        opts = {
+            defaults = {
+                ["<localleader>r"] = "REPL",
+                ["<localleader>rl"] = "Log",
+                ["<localleader>re"] = "Eval",
+                ["<localleader>rec"] = "Eval and Comment",
+                ["<localleader>rg"] = "Go",
+            },
         },
     },
-    init = function()
-        vim.g["conjure#mapping#prefix"] = "<localleader>r"
-        require("which-key").register({
-            ["<localleader>"] = {
-                r = {
-                    name = "REPL",
-                    l = {
-                        name = "REPL Log",
-                        s = "Log Split",
-                        v = "Log Vertical split",
-                        t = "Log Tab",
-                        b = "Log Buffer",
-                        g = "Log toggle",
-                        r = "Log Reset soft",
-                        R = "Log Reset hard",
-                        l = "Log jump Latest",
-                        q = "Log close visible",
-                    },
-                    e = {
-                        name = "REPL Eval",
-                        e = "Eval",
-                        c = {
-                            name = "Eval and Comment",
-                            e = "Eval and Comment",
-                            r = "Eval Root and Comment",
-                            w = "Eval Word and Comment",
+    {
+        "Olical/conjure",
+        version = "",
+        cmd = { "ConjureEval", "ConjureSchool", "ConjureConnect", "ConjureClientState" },
+        keys = {
+            { "<localleader>rlv", desc = "Log Split" },
+            { "<localleader>rlb", desc = "Log Vertical split" },
+            { "<localleader>rlt", desc = "Log Tab" },
+            { "<localleader>rlb", desc = "Log Buffer" },
+            { "<localleader>rlg", desc = "Log toggle" },
+            { "<localleader>rlr", desc = "Log Reset soft" },
+            { "<localleader>rlR", desc = "Log Reset hard" },
+            { "<localleader>rll", desc = "Log jump Latest" },
+            { "<localleader>rlq", desc = "Log close visible" },
+            { "<localleader>rec", desc = "Eval and Comment" },
+            { "<localleader>recr", desc = "Eval Root and Comment" },
+            { "<localleader>recw", desc = "Eval Word and Comment" },
+            { "<localleader>rew", desc = "Eval Word" },
+            { "<localleader>re!", desc = "Eval and replace" },
+            { "<localleader>rem", desc = "Eval Mark" },
+            { "<localleader>ref", desc = "Eval File from disk" },
+            { "<localleader>reb", desc = "Eval buffer" },
+            { "<localleader>rgd", desc = "Go Definition" },
+
+            { "<localleader>rE", desc = "Eval visual", mode = { "v" } },
+        },
+        dependencies = {
+            {
+                "PaterJason/cmp-conjure",
+                config = function()
+                    local cmp = require("cmp")
+                    local config = cmp.get_config()
+                    table.insert(config.sources, {
+                        name = "buffer",
+                        option = {
+                            sources = {
+                                { name = "conjure" },
+                            },
                         },
-                        w = "Eval Word",
-                        ["!"] = "Eval and replace",
-                        m = "Eval Mark",
-                        f = "Eval File from disk",
-                        b = "Eval buffer",
-                        E = "Eval visual",
-                    },
-                    g = {
-                        name = "REPL Go",
-                        d = "Go Definition",
-                    },
-                },
+                    })
+                    cmp.setup(config)
+                end,
             },
-        })
-    end,
+        },
+        init = function()
+            vim.g["conjure#mapping#prefix"] = "<localleader>r"
+            vim.g["conjure#mapping#log_split"] = "lv"
+            vim.g["conjure#mapping#log_vsplit"] = "lb"
+        end,
+    },
 }
