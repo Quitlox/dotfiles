dd045ff1aee451395a7eb011c6a6e5224381c53c
Kevin Witlox <kevin.witlox@upcmail.nl>
2024-01-07 21:50:07 +0100

    [nvim] improve iron.nvim config and add molten.nvim

--- private_dot_config/exact_nvim/exact_lua/exact_quitlox/exact_plugins/exact_ide/exact_lang/python.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_quitlox/exact_plugins/exact_ide/exact_lang/python.lua
@@ -169,52 +169,6 @@ return {
     --  +----------------------------------------------------------+
 
     {
-        "Vigemus/iron.nvim",
-        lazy = false,
-        config = function()
-            require("iron.core").setup({
-                config = {
-                    -- Whether a repl should be discarded or not
-                    scratch_repl = true,
-                    -- Your repl definitions come here
-                    repl_definition = {
-                        sh = {
-                            -- Can be a table or a function that
-                            -- returns a table (see below)
-                            command = { "zsh" },
-                        },
-                    },
-                    -- How the repl window will be displayed
-                    -- See below for more information
-                    repl_open_cmd = require("iron.view").bottom(40),
-                },
-                -- Iron doesn't set keymaps by default anymore.
-                -- You can set them here or manually add keymaps to the functions in iron.core
-                keymaps = {
-                    send_motion = "<space>rc",
-                    visual_send = "<space>rc",
-                    send_file = "<space>rf",
-                    send_line = "<space>rl",
-                    send_until_cursor = "<space>ru",
-                    send_mark = "<space>rm",
-                    mark_motion = "<space>rmc",
-                    mark_visual = "<space>rmc",
-                    remove_mark = "<space>rmd",
-                    cr = "<space>r<cr>",
-                    interrupt = "<space>rx",
-                    exit = "<space>rq",
-                    clear = "<space>rl",
-                },
-                -- If the highlight is on, you can change how it looks
-                -- For the available options, check nvim_set_hl
-                highlight = {
-                    italic = true,
-                },
-                ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
-            })
-        end,
-    },
-    {
         "folke/which-key.nvim",
         opts = {
             defaults = {
