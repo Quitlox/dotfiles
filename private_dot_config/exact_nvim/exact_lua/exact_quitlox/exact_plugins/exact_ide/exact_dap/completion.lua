ed67798b7149a72bc6bb4e8c8428f1f46febaa93
Kevin Witlox <kevin.witlox@upcmail.nl>
2024-01-07 21:48:59 +0100

    [nvim] remove erroneous binding in completion.lua

--- private_dot_config/exact_nvim/exact_lua/exact_quitlox/exact_plugins/exact_ide/exact_dap/completion.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_quitlox/exact_plugins/exact_ide/exact_dap/completion.lua
@@ -1,12 +1,9 @@
 return {
     "rcarriga/cmp-dap",
     dependencies = { "hrsh7th/nvim-cmp" },
-    keys = "<localleader>d",
     config = function()
         require("cmp").setup({
-            enabled = function()
-                return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt" or require("cmp_dap").is_dap_buffer()
-            end,
+            enabled = function() return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt" or require("cmp_dap").is_dap_buffer() end,
         })
     end,
 }
