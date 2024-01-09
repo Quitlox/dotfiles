2afded981b24598ab51f13105c32e1330c4d44a0
Kevin Witlox <kevin.witlox@tno.nl>
2024-01-09 15:11:50 +0100

    [nvim] remove overlapping keybind of diagnostic_help

--- private_dot_config/exact_nvim/exact_lua/exact_quitlox/exact_plugins/exact_ide/exact_lsp/diagnostic_help.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_quitlox/exact_plugins/exact_ide/exact_lsp/diagnostic_help.lua
@@ -3,7 +3,7 @@ return {
     dependencies = { "MunifTanjim/nui.nvim" },
     opts = {},
     keys = {
-        { "gw", mode = { "n", "x" }, function() require("wtf").ai() end, desc = "Debug diagnostic with AI" },
-        { "gW", mode = { "n" }, function() require("wtf").search() end, desc = "Search diagnostic with Google" },
+        { "gs", mode = { "n", "x" }, function() require("wtf").ai() end, desc = "Debug diagnostic with AI" },
+        { "gS", mode = { "n" }, function() require("wtf").search() end, desc = "Search diagnostic with Google" },
     },
 }
