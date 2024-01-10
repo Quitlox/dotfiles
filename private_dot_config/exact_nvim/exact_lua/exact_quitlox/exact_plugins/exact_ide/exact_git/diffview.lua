fb919c09a6d40d39ff5701401811788923e4daa6
Kevin Witlox <kevin.witlox@tno.nl>
2024-01-10 11:26:47 +0100

    [nvim] lazy load diffview.nvim

--- private_dot_config/exact_nvim/exact_lua/exact_quitlox/exact_plugins/exact_ide/exact_git/diffview.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_quitlox/exact_plugins/exact_ide/exact_git/diffview.lua
@@ -10,32 +10,24 @@ return {
         keys = {
             { "<leader>gd", "<cmd>DiffviewFileHistory<cr>", desc = "Git Diff File History" },
         },
-        lazy = false,
-        -- cmd = {
-        --     "DiffviewFileHistory",
-        --     "DiffviewOpen",
-        --     "DiffviewClose",
-        --     "DiffviewToggleFiles",
-        --     "DiffviewFocusFiles",
-        --     "DiffviewRefresh",
-        -- },
+        cmd = {
+            "DiffviewFileHistory",
+            "DiffviewOpen",
+            "DiffviewClose",
+            "DiffviewToggleFiles",
+            "DiffviewFocusFiles",
+            "DiffviewRefresh",
+        },
     },
-    {
-        "mrjones2014/legendary.nvim",
-        optional = true,
-        opts = function(_, opts)
-            opts.commands = opts.commands or {}
-            vim.list_extend(opts.commands, {
-                { "DiffviewFileHistory", description = "Diffview File History" },
-                { "DiffviewOpen", description = "Diffview Open (compare against current index)" },
-                { "DiffviewClose", description = "Diffview Close" },
-                { "DiffviewToggleFiles", description = "Diffview Toggle files" },
-                { "DiffviewFocusFiles", description = "Diffview Locate (focus) files" },
-                { "DiffviewRefresh", description = "Diffview Refresh" },
-                -- See https://github.com/sindrets/diffview.nvim/blob/main/USAGE.md 
-                { "DiffviewReviewPR", "DiffviewOpen origin/HEAD...HEAD --imply-local", description = "Diffview to review a Pull Request"},
-                { "DiffviewReviewPRByCommit", "DiffviewFileHistory --range=origin/HEAD...HEAD --right-only --no-merges", description = "Diffview to review a Pull Request"},
-            })
-        end,
-    },
+    require("quitlox.util").legendary_full({
+        { ":DiffviewFileHistory", description = "Diffview File History" },
+        { ":DiffviewOpen", description = "Diffview Open (compare against current index)" },
+        { ":DiffviewClose", description = "Diffview Close" },
+        { ":DiffviewToggleFiles", description = "Diffview Toggle files" },
+        { ":DiffviewFocusFiles", description = "Diffview Locate (focus) files" },
+        { ":DiffviewRefresh", description = "Diffview Refresh" },
+        -- See https://github.com/sindrets/diffview.nvim/blob/main/USAGE.md
+        { ":DiffviewReviewPR", "DiffviewOpen origin/HEAD...HEAD --imply-local", description = "Diffview to review a Pull Request" },
+        { ":DiffviewReviewPRByCommit", "DiffviewFileHistory --range=origin/HEAD...HEAD --right-only --no-merges", description = "Diffview to review a Pull Request" },
+    }),
 }
