44231b5daaef1ba9afadeae0ad85871f07f3baee
Kevin Witlox <kevin.witlox@upcmail.nl>
2024-01-13 20:38:38 +0100

    [nvim] remove symbols usage

--- private_dot_config/exact_nvim/exact_lua/exact_quitlox/exact_plugins/exact_ide/exact_lsp/symbol_usage.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "Wansmer/symbol-usage.nvim",
-    event = "BufReadPre", -- need run before LspAttach if you use nvim 0.9. On 0.10 use 'LspAttach'
-    opts = {},
-    enabled = false,
-    init = function()
-        vim.cmd [[command! -nargs=0 SymbolUsageToggle lua require('symbol-usage').toggle_globally()]]
-    end,
-}
