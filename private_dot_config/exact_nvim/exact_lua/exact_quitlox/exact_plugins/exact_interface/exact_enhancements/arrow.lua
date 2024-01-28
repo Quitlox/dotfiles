207b2ad044304004d35ded906699f87c2e916fe9
Kevin Witlox <kevin.witlox@tno.nl>
2024-01-28 11:10:46 +0100

    [nvim] add arrow plugin

---
+++ private_dot_config/exact_nvim/exact_lua/exact_quitlox/exact_plugins/exact_interface/exact_enhancements/arrow.lua
@@ -0,0 +1,12 @@
+return {
+    "otavioschwanck/arrow.nvim",
+    keys = { ";" },
+    opts = {
+        show_icons = true,
+        leader_key = ";",
+        mappings = {
+            open_vertical = "b",
+            open_horizontal = "v",
+        },
+    },
+}
