76a41d1bc9f966e6b6cc164507d0fcb30df7505f
Kevin Witlox <kevin.witlox@tno.nl>
2024-04-16 14:12:20 +0200

    [nvim] finally fix on windows!

--- AppData/Local/nvim/init.lua
+++
@@ -1,2 +0,0 @@
-vim.api.nvim_command('set runtimepath^=C:/Users/witloxkhd/.config/vim')
-vim.api.nvim_exec("source ~/.config/vim/vimrc", false)
---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,26 @@
+# Aliases
+Function alias_chm { z "C:\Users\witloxkhd\.local\share\chezmoi" }
+Set-Alias "chm" alias_chm
+Function alias_dotdot { cd .. }
+Set-Alias ".." alias_dotdot 
+Function alias_l { ls }
+Set-Alias "l" alias_l
+# Aliases - Git
+Function alias_gs { git status $args }
+Set-Alias "gs" alias_gs
+Function alias_gpl { git pull $args }
+Set-Alias "gpl" alias_gpl
+Function alias_ga { git add $args }
+Set-Alias "ga" alias_ga 
+
+# Zoxide
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
+Function alias_cd { z $args }
+Set-Alias "cd" alias_cd -Option AllScope
+
+# Neovim
+$env:XDG_CONFIG_HOME="C:/Users/witloxkhd/.config"
+
+# Completion
+Set-PSReadlineKeyHandler -Chord Tab -Function MenuComplete
+Set-PSReadlineOption -BellStyle None
