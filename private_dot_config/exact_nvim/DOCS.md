91da18d5c12cd3f2cf30766bf01e8fc2294f4fb9
Kevin Witlox <kevin.witlox@upcmail.nl>
2024-06-20 10:04:08 +0200

    [nvim] move wtf.nvim to archive

--- private_dot_config/exact_nvim/DOCS.md
+++ private_dot_config/exact_nvim/DOCS.md
@@ -1,6 +1,14 @@
-
 ## Plugin Graveyard
 
 Plugins that could be useful, but I am not using.
-- `chrisgrieser/nvim-chainsaw`: Easily insert different types of logging statements in any language.
-- `ecthelionvi/NeoComposer.nvim`: Simplifies macros, enhancing productivity with harmony.
+- `chrisgrieser/nvim-chainsaw`: Easily insert different types of logging
+  statements in any language.
+- `ecthelionvi/NeoComposer.nvim`: Simplifies macros, enhancing productivity with
+  harmony.
+- `piersolenski/wtf.nvim` (`diagnostic_help.lua`): Easily search diagnostics on
+  the internet or query GPT.
+
+## Plugins with Potential
+
+- `SleepySwords/change-function.nvim`: Uses treesitter and LSP to change
+  function parameter order globally.
---
+++ private_dot_config/exact_nvim/exact_archive/diagnostic_help.lua
@@ -0,0 +1,26 @@
+return {
+    {
+        "piersolenski/wtf.nvim",
+        dependencies = { "MunifTanjim/nui.nvim" },
+        opts = {},
+        keys = {
+            -- stylua: ignore start
+            -- { "gw", mode = { "n", "x" }, function() require("wtf").ai() end, desc = "Debug diagnostic with AI" },
+            -- { "gW", mode = { "n" }, function() require("wtf").search() end, desc = "Search diagnostic with Google" },
+            -- stylua: ignore end
+        },
+    },
+    {
+        "mrjones2014/legendary.nvim",
+        optional = true,
+        opts = function(_, opts)
+            opts.commands = opts.commands or {}
+            vim.list_extend(opts.commands, {
+                -- stylua: ignore start
+                { ":DebugDiagnosticWithAI", function() require("wtf").ai() end, description = "wtf.nvim: Debug diagnostic with ai" },
+                { ":SearchDiagnosticWithGoogle", function() require("wtf").search() end,description = "wtf.nvim: Search diagnostic with google" },
+                -- stylua: ignore end
+            })
+        end,
+    },
+}
