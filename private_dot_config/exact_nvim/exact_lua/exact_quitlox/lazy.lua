fcc9b2806b5bf157187c0ff297d224b94ace8e82
Kevin Witlox <kevin.witlox@upcmail.nl>
2024-06-20 10:05:24 +0200

    [nvim] add profile.nvim

--- private_dot_config/exact_nvim/exact_lua/exact_quitlox/lazy.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_quitlox/lazy.lua
@@ -8,13 +8,15 @@ vim.loader.enable()
 ----------------------------------------------------------------------
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    lazypath,
-}) end
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        lazypath,
+    })
+end
 vim.opt.rtp:prepend(lazypath)
 
 ----------------------------------------------------------------------
@@ -75,6 +77,7 @@ local plugins = {
     { "kylechui/nvim-surround", version = "*", event = "VeryLazy", config = true },
     { "tpope/vim-repeat" },
     ---------- Navigation ----------
+    "stevearc/profile.nvim",
 
     ----------------------------------------
     -- Import
@@ -91,3 +94,33 @@ local plugins = {
 
 require("lazy").setup(plugins, lazy_config)
 require("quitlox.config")
+
+----------------------------------------------------------------------
+--                             Profiler                             --
+----------------------------------------------------------------------
+
+local should_profile = os.getenv("NVIM_PROFILE")
+if should_profile then
+    require("profile").instrument_autocmds()
+    if should_profile:lower():match("^start") then
+        require("profile").start("*")
+    else
+        require("profile").instrument("*")
+    end
+end
+
+local function toggle_profile()
+    local prof = require("profile")
+    if prof.is_recording() then
+        prof.stop()
+        vim.ui.input({ prompt = "Save profile to:", completion = "file", default = "profile.json" }, function(filename)
+            if filename then
+                prof.export(filename)
+                vim.notify(string.format("Wrote %s", filename))
+            end
+        end)
+    else
+        prof.start("*")
+    end
+end
+vim.keymap.set("", "<f1>", toggle_profile)
