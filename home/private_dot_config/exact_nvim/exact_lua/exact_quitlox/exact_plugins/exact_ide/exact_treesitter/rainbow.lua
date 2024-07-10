15b5afdf132e098da839f5ab0934ce60c818d785
Kevin Witlox <kevin.witlox@upcmail.nl>
2024-07-10 20:34:19 +0200

    [nvim] temporarily disable rainbow-delimiters

--- home/private_dot_config/exact_nvim/exact_lua/exact_quitlox/exact_plugins/exact_ide/exact_treesitter/rainbow.lua
+++ home/private_dot_config/exact_nvim/exact_lua/exact_quitlox/exact_plugins/exact_ide/exact_treesitter/rainbow.lua
@@ -1,5 +1,11 @@
 return {
     url = "https://gitlab.com/HiPhish/rainbow-delimiters.nvim",
     version = "",
+    -- FIXME: BUG. Something is wrong in the query for helpdocs. Steps to reproduce/test:
+    --      - ex: "h lazy.nvim.txt"
+    --      - search for after using: "/after"
+    --      - hit next five times (all occurences)
+    --      - > error
+    enabled = false,
     lazy = false,
 }
