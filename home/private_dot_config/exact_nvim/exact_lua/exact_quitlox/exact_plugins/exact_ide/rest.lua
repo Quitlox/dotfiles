3ac8713fc2030c9f5282dcc1d75d6cd47de75f16
Kevin Witlox <kevin.witlox@tno.nl>
2024-07-24 17:20:28 +0200

    [nvim] swap rest with kululu

--- home/private_dot_config/exact_nvim/exact_lua/exact_quitlox/exact_plugins/exact_ide/rest.lua
+++ home/private_dot_config/exact_nvim/exact_lua/exact_quitlox/exact_plugins/exact_ide/rest.lua
@@ -1,13 +1,34 @@
 return {
-    "aquach/vim-http-client",
+    "mistweaverco/kulala.nvim",
+    config = function()
+        -- Setup is required, even if you don't pass any options
+        require("kulala").setup()
+    end,
     ft = "http",
     init = function()
-        vim.g.http_client_bind_hotkey = 0
-        vim.g.http_client_json_ft = "vim-json"
-        vim.g.http_client_focus_output_window = 0
-        vim.g.http_client_preserve_responses = 1
+        vim.filetype.add({
+            extension = {
+                ["http"] = "http",
+            },
+        })
+
+        vim.api.nvim_create_autocmd("FileType", {
+            group = vim.api.nvim_create_augroup("MyHttpOptions", { clear = true }),
+            pattern = { "http" },
+            callback = function(event) vim.opt_local.formatoptions:remove({ "r" }) end,
+        })
+
+        vim.api.nvim_create_autocmd("FileType", {
+            group = vim.api.nvim_create_augroup("MyHttpKeybinding", { clear = true }),
+            pattern = { "http" },
+            callback = function(event)
+                vim.keymap.set("n", "<localleader>hr", "<cmd>lua require('kulala').run()<cr>", { buffer = event.buf, silent = true, nowait = true })
+                vim.keymap.set("n", "K", "<cmd>lua require('kulala').run()<cr>", { buffer = event.buf, silent = true, nowait = true })
+                vim.keymap.set("n", "<localleader>hc", "<cmd>lua require('kulala').copy()<cr>", { buffer = event.buf, silent = true, nowait = true })
+                vim.keymap.set("n", "<localleader>ht", "<cmd>lua require('kulala').toggle_view()<cr>", { buffer = event.buf, silent = true, nowait = true })
+                vim.keymap.set("n", "]h", "<cmd>lua require('kulala').jump_next()<cr>", { buffer = event.buf, silent = true, nowait = true })
+                vim.keymap.set("n", "[h", "<cmd>lua require('kulala').jump_prev()<cr>", { buffer = event.buf, silent = true, nowait = true })
+            end,
+        })
     end,
-    keys = {
-        { "<localleader>hr", "<cmd>HTTPClientDoRequest<cr>", desc = "Run HTTP request under cursor" },
-    },
 }
