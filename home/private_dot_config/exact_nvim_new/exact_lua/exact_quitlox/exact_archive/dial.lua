28f46b7f9e9470973080218bab1c445b95bfc393
Kevin Witlox <kevin.witlox@upcmail.nl>
2024-07-29 17:09:16 +0200

    [nvim_new] remove deleted files

--- home/private_dot_config/exact_nvim_new/exact_lua/exact_quitlox/exact_archive/dial.lua
+++
@@ -1,8 +0,0 @@
--- +---------------------------------------------------------+
--- | monaqa/dial.nvim: Enhanced Increment / Decrement        |
--- +---------------------------------------------------------+
-
-vim.keymap.set("n", "<C-a>", require("dial.map").inc_normal(), { noremap = true })
-vim.keymap.set("n", "<C-x>", require("dial.map").dec_normal(), { noremap = true })
-vim.keymap.set("v", "<C-a>", require("dial.map").inc_visual(), { noremap = true })
-vim.keymap.set("v", "<C-x>", require("dial.map").dec_visual(), { noremap = true })
--- home/private_dot_config/exact_nvim_new/exact_lua/exact_quitlox/exact_plugins/colorizer.lua
+++
@@ -1,8 +0,0 @@
-require("colorizer").setup({
-    user_default_options = {
-        RGB = false,
-        RRGGBBAA = true,
-        names = false,
-        tailwind = false,
-    },
-})
