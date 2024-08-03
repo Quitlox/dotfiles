b7f9f28a24b4256fc4e04cf07597f3d2ff3dd0ba
Kevin Witlox <kevin.witlox@upcmail.nl>
2024-08-03 13:51:02 +0200

    [nvim] markdown preview should only install once

--- home/private_dot_config/exact_nvim/exact_lua/exact_quitlox/exact_plugins/exact_lang/markdown-preview.lua
+++ home/private_dot_config/exact_nvim/exact_lua/exact_quitlox/exact_plugins/exact_lang/markdown-preview.lua
@@ -2,12 +2,17 @@
 -- | iamcco/markdown-preview.nvim: Preview Markdown in Browser|
 -- +---------------------------------------------------------+
 
+-- Install
+vim.api.nvim_create_autocmd({ "FileType" }, {
+    pattern = "markdown",
+    once = true,
+    callback = function() vim.fn["mkdp#util#install"]() end,
+})
+
 --+- Keymaps ------------------------------------------------+
 vim.api.nvim_create_autocmd({ "FileType" }, {
     pattern = "markdown",
     callback = function()
-        vim.fn["mkdp#util#install"]()
-
         local bufnr = vim.api.nvim_get_current_buf()
         vim.keymap.set("n", "<localleader>mo", "<cmd>MarkdownPreview<cr>", { noremap = true, silent = true, desc = "Markdown Preview", buffer = bufnr })
         vim.keymap.set("n", "<localleader>mb", "<cmd>MarkdownPreviewStop<cr>", { noremap = true, silent = true, desc = "Markdown Preview Stop", buffer = bufnr })
