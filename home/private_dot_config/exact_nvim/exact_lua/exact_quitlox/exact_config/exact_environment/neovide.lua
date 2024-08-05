72744554e573726dcffa44fb96e07b4c9687d22d
Kevin Witlox <kevin.witlox@upcmail.nl>
2024-08-05 08:47:59 +0200

    [neovide] no more nerd font

--- home/private_dot_config/exact_nvim/exact_lua/exact_quitlox/exact_config/exact_environment/neovide.lua
+++ home/private_dot_config/exact_nvim/exact_lua/exact_quitlox/exact_config/exact_environment/neovide.lua
@@ -1,6 +1,4 @@
-if vim.fn.exists("g:neovide") == 0 then
-	return
-end
+if vim.fn.exists("g:neovide") == 0 then return end
 
-vim.o.guifont = "Iosevka Nerd Font"
+vim.o.guifont = "Iosevka:h12"
 vim.g.neovide_window_blurred = true
