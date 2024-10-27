0ecb1fb3c2ab8920a2cf43e5bd308a2bc0a38b41
Kevin Witlox <kevin.witlox@upcmail.nl>
2024-10-28 00:05:44 +0100

    [nvim] disable matchup for svelte

--- home/private_dot_config/exact_nvim/exact_lua/exact_quitlox/exact_plugins/exact_editor/matchup.lua
+++ home/private_dot_config/exact_nvim/exact_lua/exact_quitlox/exact_plugins/exact_editor/matchup.lua
@@ -6,6 +6,6 @@
 require("nvim-treesitter.configs").setup({
     matchup = {
         enable = true,
-        disable = { "tsx", "svelte" },
+        disable = { "tsx", "svelte" }, -- FIXME: Having problems
     },
 })
