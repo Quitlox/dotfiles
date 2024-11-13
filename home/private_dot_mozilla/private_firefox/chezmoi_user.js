78b61af567a405c864a177a9c2ce96533a1ece24
Kevin Witlox <kevin.witlox@upcmail.nl>
2024-11-13 07:59:07 +0100

    [firefox] configure user.js

--- home/private_dot_mozilla/private_firefox/chezmoi_user.js
+++ home/private_dot_mozilla/private_firefox/chezmoi_user.js
@@ -1,2 +1,4 @@
 user_pref("browser.aboutConfig.showWarning", false);
 user_pref("browser.fullscreen.autohide", false);
+user_pref("browser.translations.neverTranslateLanguages", "nl");
+user_pref("services.sync.engine.passwords", false);
