87d74ccc76d74b5ad06a9e808ca90ed9c0e1e569
Kevin Witlox <kevin.witlox@upcmail.nl>
2025-03-15 19:19:04 +0100

    [firefox] fix bug in script

--- home/private_dot_mozilla/private_firefox/run_after_mozilla_configuration.sh
+++ home/private_dot_mozilla/private_firefox/run_after_mozilla_configuration.sh
@@ -5,12 +5,14 @@ FIREFOX_PROFILE_PREFIX="$HOME/.mozilla/firefox"
 USER_JS_SOURCE="./chezmoi_user.js"
 
 # Find the directory matching '*.default-release'
-profiles=("$FIREFOX_PROFILE_PREFIX"/*.default-release)
+shopt -s nullglob
+profiles=($FIREFOX_PROFILE_PREFIX/*.default-release)
+shopt -u nullglob
 
 # Ensure that exactly one `.default.release` directory was found
 if [ ${#profiles[@]} -ne 1 ]; then
-    echo "Error: Found ${#profiles[@]} directories ending with '.default-release'. There should be exactly one."
-    exit 1
+    echo "Warning: Found ${#profiles[@]} directories ending with '.default-release'. There should be exactly one. Skiping user.js configuration."
+    exit 0
 fi
 
 # Check if user.js exists at the source location
