eacfe60541bfa8b4a7e821a76d95bd16ce1855cd
Kevin Witlox <kevin.witlox@upcmail.nl>
2025-03-29 20:42:34 +0100

    [nvim] feat: add chezmoi syntax highlighting

---
+++ home/private_dot_config/exact_nvim/exact_lua/exact_quitlox/exact_plugins/chezmoi-vim.lua
@@ -0,0 +1,6 @@
+-- +---------------------------------------------------------+
+-- | alker0/chezmoi.vim: Chezmoi Syntax Highlighting         |
+-- +---------------------------------------------------------+
+
+vim.g["chezmoi#use_tmp_buffer"] = 1
+vim.g["chezmoi#source_dir_path"] = os.getenv("HOME") .. "/.local/share/chezmoi"
