a45d5e32e01a9d89886a8914a59271fb3d6c35ca
Kevin Witlox <kevin.witlox@upcmail.nl>
2025-03-29 20:48:53 +0100

    [nvim] fix(lualine): bug in searchcount component

--- home/private_dot_config/exact_nvim/exact_lua/exact_lualine/exact_components/my_fancy_searchcount.lua
+++ home/private_dot_config/exact_nvim/exact_lua/exact_lualine/exact_components/my_fancy_searchcount.lua
@@ -6,12 +6,20 @@ function M:init(options)
 end
 
 function M:update_status()
-    local search = vim.fn.searchcount({ maxcount = 0 })
-    if next(search) ~= nil then
+    -- Add pcall to catch any error from searchcount()
+    local success, search = pcall(function()
+        return vim.fn.searchcount({ maxcount = 0 })
+    end)
+
+    -- Only proceed if the call was successful
+    if success and search and next(search) ~= nil then
         if search.current > 0 and vim.v.hlsearch ~= 0 then
             return search.current .. "/" .. search.total
         end
     end
+
+    -- Return empty string if there was an error or no valid search
+    return ""
 end
 
 return M
