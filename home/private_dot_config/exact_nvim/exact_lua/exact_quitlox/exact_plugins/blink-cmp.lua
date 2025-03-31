6740fadaa047e24721f9974b0f1376a50ffa58d1
Kevin Witlox <kevin.witlox@upcmail.nl>
2025-03-31 17:59:50 +0200

    [nvim] add colorful-menu

--- home/private_dot_config/exact_nvim/exact_lua/exact_quitlox/exact_plugins/blink-cmp.lua
+++ home/private_dot_config/exact_nvim/exact_lua/exact_quitlox/exact_plugins/blink-cmp.lua
@@ -50,7 +50,16 @@ require("blink-cmp").setup({
         -- Setup mini.icons
         menu = {
             draw = {
+                columns = { { "kind_icon" }, { "label", gap = 1 } },
                 components = {
+                    label = {
+                        text = function(ctx)
+                            return require("colorful-menu").blink_components_text(ctx)
+                        end,
+                        highlight = function(ctx)
+                            return require("colorful-menu").blink_components_highlight(ctx)
+                        end,
+                    },
                     kind_icon = {
                         ellipsis = false,
                         text = function(ctx)
