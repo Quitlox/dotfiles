92bcfcd1cdc37de0dd73bbb9d31fe412468e9558
Kevin Witlox <kevin.witlox@upcmail.nl>
2025-05-07 20:12:24 +0200

    feat(nvim): add conceal for \mod and friends to markview

--- home/private_dot_config/exact_nvim/exact_lua/exact_quitlox/exact_plugins/markview.lua
+++ home/private_dot_config/exact_nvim/exact_lua/exact_quitlox/exact_plugins/markview.lua
@@ -17,7 +17,94 @@ require("markview").setup({
         headings = { shift_width = 0 },
     },
     markdown = {},
-    latex = {},
+    latex = {
+        -- TODO: If these work well, I should contribute them to the main repo (in lua/markview/spec.lua)
+        commands = {
+            -- Long space before "mod"
+            ["mod"] = {
+                condition = function(item)
+                    return #item.args == 1
+                end,
+                on_command = {
+                    conceal = "",
+                    virt_text_pos = "inline",
+                    virt_text = { { "   mod ", "@keyword.function" } },
+                    hl_mode = "combine",
+                },
+                on_args = {
+                    {
+                        on_before = function(item)
+                            return {
+                                end_col = item.range[2] + 1,
+                                virt_text_pos = "overlay",
+                                virt_text = { { "(", "@punctuation.bracket" } },
+                                hl_mode = "combine",
+                            }
+                        end,
+                        after_offset = function(range)
+                            return { range[1], range[2], range[3], range[4] - 1 }
+                        end,
+                        on_after = function(item)
+                            return {
+                                end_col = item.range[4],
+                                virt_text_pos = "overlay",
+                                virt_text = { { ")", "@punctuation.bracket" } },
+                                hl_mode = "combine",
+                            }
+                        end,
+                    },
+                },
+            },
+
+            -- Short space before "mod"
+            ["bmod"] = {
+                condition = function(item)
+                    return true
+                end,
+                on_command = {
+                    conceal = "",
+                    virt_text_pos = "inline",
+                    virt_text = { { " mod ", "@keyword.function" } },
+                    hl_mode = "combine",
+                },
+            },
+
+            -- (mod ...) format
+            ["pmod"] = {
+                condition = function(item)
+                    return #item.args == 1
+                end,
+                on_command = {
+                    conceal = "",
+                },
+                on_args = {
+                    {
+                        on_before = function(item)
+                            return {
+                                end_col = item.range[2] + 1,
+                                conceal = "",
+                                virt_text_pos = "inline",
+                                virt_text = { { " (mod ", "@keyword.function" } },
+                                hl_mode = "combine",
+                            }
+                        end,
+                        after_offset = function(range)
+                            return { range[1], range[2], range[3], range[4] - 1 }
+                        end,
+                        on_after = function(item)
+                            return {
+                                end_col = item.range[4],
+                                conceal = "",
+                                virt_text_pos = "inline",
+                                virt_text = { { ")", "@punctuation.bracket" } },
+                                hl_mode = "combine",
+                            }
+                        end,
+                    },
+                },
+            },
+        },
+    },
 })
 
 -- Load the checkboxes module.
