75911d89e5dc0fd3ceb3e879476a1ae7296e3efc
Kevin Witlox <kevin.witlox@tno.nl>
2025-05-08 09:26:28 +0200

    fix(pwsh): attempt to add bash-like keybindings

--- home/Documents/PowerShell/Profile.ps1
+++ home/Documents/PowerShell/Profile.ps1
@@ -13,14 +13,21 @@ Set-Alias "gpl" alias_gpl
 Function alias_ga { git add $args }
 Set-Alias "ga" alias_ga 
 
-# Zoxide
-Invoke-Expression (& { (zoxide init powershell | Out-String) })
-Function alias_cd { z $args }
-Set-Alias "cd" alias_cd -Option AllScope
-
 # Neovim
 $env:XDG_CONFIG_HOME="~/.config"
 
 # Completion
 Set-PSReadlineKeyHandler -Chord Tab -Function MenuComplete
+Set-PSReadlineOption -PredictionSource History
 Set-PSReadlineOption -BellStyle None
+
+# Keybindings
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineKeyHandler -Chord Ctrl+E -Function ForwardWord
+Set-PSReadLineKeyHandler -Chord Ctrl+P -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Chord Ctrl+N -Function HistorySearchForward
+
+# Zoxide
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
+# Function alias_cd { z $args }
+# Set-Alias "cd" alias_cd -Option AllScope
