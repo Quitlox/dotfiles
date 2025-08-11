e2f5a6ceb3ac373e83b5412072281097636367b6
Kevin Witlox <kevin.witlox@upcmail.nl>
2025-08-11 22:48:10 +0200

    fix(nvim): disable copilot by default

--- home/private_dot_config/exact_nvim/exact_lua/exact_quitlox/exact_plugins/copilot.lua
+++ home/private_dot_config/exact_nvim/exact_lua/exact_quitlox/exact_plugins/copilot.lua
@@ -2,6 +2,7 @@
 -- | github/copilot.vim: Copilot                             |
 -- +---------------------------------------------------------+
 
+vim.g.copilot_enabled = false
 vim.g.copilot_filetypes = { Telescope = false, octo = false, tex = false, latex = false }
 vim.g.copilot_no_tab_map = true
 vim.cmd([[imap <silent><script><expr> <c-a> copilot#Accept("")]])
