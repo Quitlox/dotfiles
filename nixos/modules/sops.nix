a0db65f446448f8b12290a81b21565eb7697f572
Kevin Witlox <kevin.witlox@upcmail.nl>
2025-08-19 21:41:38 +0200

    fix(nix): add documentation to sops

--- nixos/modules/sops.nix
+++ nixos/modules/sops.nix
@@ -1,3 +1,18 @@
+# SOPS
+#
+# SOPS (Secrets OPerationS) is used to store secrets for use in NixOS configuration.
+# The secrets are stored in encrypted form in the doftiles repository in /nixos/secrets
+# 
+# To edit the secrets:
+#     > `sops nixos/secrets/secrets.yaml`
+#
+# If the SSH key of the machine is not onboarded, use the personal private key:
+#     > `export SOPS_AGE_KEY_FILE=~/.ssh/.age_private_key.txt`
+#
+# Currently onboarded keys:
+# - quitlox-pi "/etc/ssh/ssh_host_ed25519_key"
+# - .age_private_key.txt
+
 {
   config,
   pkgs,
