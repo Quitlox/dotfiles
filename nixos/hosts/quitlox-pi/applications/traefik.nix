df512cb045fb262b3db70b91b76aa47332f8848a
Kevin Witlox <kevin.witlox@upcmail.nl>
2025-08-22 18:39:29 +0200

    fix(nix): remove ssl from traefik

--- nixos/hosts/quitlox-pi/applications/traefik.nix
+++ nixos/hosts/quitlox-pi/applications/traefik.nix
@@ -4,20 +4,13 @@
     enable = true;
 
     staticConfigOptions = {
+      api.dashboard = true;
+      api.insecure = true;
+
       entryPoints = {
         web = {
           address = ":80";
           asDefault = true;
-          http.redirections.entrypoint = {
-            to = "websecure";
-            scheme = "https";
-          };
-        };
-
-        websecure = {
-          address = ":443";
-          asDefault = true;
-          http.tls.certResolver = "letsencrypt";
         };
       };
 
@@ -26,17 +19,6 @@
         filePath = "${config.services.traefik.dataDir}/traefik.log";
         format = "json";
       };
-
-      certificatesResolvers.letsencrypt.acme = {
-        email = "kevin.witlox@upcmail.nl";
-        storage = "${config.services.traefik.dataDir}/acme.json";
-        httpChallenge.entryPoint = "web";
-        caServer = "https://acme-staging-v02.api.letsencrypt.org/directory"; # use staging temporarily
-      };
-
-      api.dashboard = true;
-      # Access the Traefik dashboard on <Traefik IP>:8080 of your server
-      api.insecure = true;
     };
 
     dynamicConfigOptions = {
