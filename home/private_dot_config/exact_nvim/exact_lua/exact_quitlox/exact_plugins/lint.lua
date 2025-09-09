6a4a38006bce713b006c6cd4987d956a7f2d6638
Kevin Witlox <kevin.witlox@tno.nl>
2025-09-09 15:57:37 +0200

    fix(nvim): remove clippy from linting

--- home/private_dot_config/exact_nvim/exact_lua/exact_quitlox/exact_plugins/lint.lua
+++ home/private_dot_config/exact_nvim/exact_lua/exact_quitlox/exact_plugins/lint.lua
@@ -5,7 +5,7 @@
 require("lint").linters_by_ft = {
     -- Major Languages
     python = { "pylint", "mypy", "codespell" },
-    rust = { "clippy", "codespell" },
+    rust = { "codespell" },
 
     -- Web Development
     javascript = { "eslint_d", "codespell" },
