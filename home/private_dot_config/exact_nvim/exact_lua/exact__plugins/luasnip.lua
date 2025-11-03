b8882775afcb8c8015a662fb21b6e57f99909f94
Kevin Witlox <kevin.witlox@tno.nl>
2025-11-03 09:36:49 +0100

    fix(nvim): fix snippet session not deactivating

--- home/private_dot_config/exact_nvim/exact_lua/exact__plugins/luasnip.lua
+++ home/private_dot_config/exact_nvim/exact_lua/exact__plugins/luasnip.lua
@@ -4,3 +4,17 @@
 
 -- Load snippets
 require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./snippets" } })
+
+-- Stop snippets session upon going to normal mode
+vim.api.nvim_create_autocmd("ModeChanged", {
+    pattern = "*",
+    callback = function()
+        if
+            ((vim.v.event.old_mode == "s" and vim.v.event.new_mode == "n") or vim.v.event.old_mode == "i")
+            and require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+            and not require("luasnip").session.jump_active
+        then
+            require("luasnip").unlink_current()
+        end
+    end,
+})
