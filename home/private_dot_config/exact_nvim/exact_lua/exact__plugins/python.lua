1e65de1deee76d60213e3c70c779fcac456ed315
Kevin Witlox <kevin.witlox@tno.nl>
2025-11-04 19:35:47 +0100

    fix(nvim): move localleader keymaps

--- home/private_dot_config/exact_nvim/exact_lua/exact__plugins/python.lua
+++ home/private_dot_config/exact_nvim/exact_lua/exact__plugins/python.lua
@@ -26,30 +26,36 @@ require("python").setup(
     }
 )
 
---+- Keymaps ------------------------------------------------+
-vim.keymap.set("n", "<localleader>pv", "<cmd>Python venv pick<cr>", { desc = "python.nvim: pick venv" })
-vim.keymap.set("n", "<localleader>pi", "<cmd>Python venv install<cr>", { desc = "python.nvim: python venv install" })
-vim.keymap.set("n", "<localleader>pd", "<cmd>Python dap<cr>", { desc = "python.nvim: python run debug program" })
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "python",
+    group = vim.api.nvim_create_augroup("MyPythonKeybindings", { clear = true }),
+    callback = function(event)
+        --+- Keymaps ------------------------------------------------+
+        vim.keymap.set("n", "<localleader>v", "<cmd>Python venv pick<cr>", { desc = "python.nvim: pick venv", buffer = event.buf })
+        vim.keymap.set("n", "<localleader>i", "<cmd>Python venv install<cr>", { desc = "python.nvim: python venv install", buffer = event.buf })
+        vim.keymap.set("n", "<localleader>d", "<cmd>Python dap<cr>", { desc = "python.nvim: python run debug program", buffer = event.buf })
 
--- Test Actions
-vim.keymap.set("n", "<localleader>ptt", "<cmd>Python test<cr>", { desc = "python.nvim: python run test suite" })
-vim.keymap.set("n", "<localleader>ptm", "<cmd>Python test_method<cr>", { desc = "python.nvim: python run test method" })
-vim.keymap.set("n", "<localleader>ptf", "<cmd>Python test_file<cr>", { desc = "python.nvim: python run test file" })
-vim.keymap.set("n", "<localleader>ptdd", "<cmd>Python test_debug<cr>", { desc = "python.nvim: run test suite in debug mode." })
-vim.keymap.set("n", "<localleader>ptdm", "<cmd>Python test_method_debug<cr>", { desc = "python.nvim: run test method in debug mode." })
-vim.keymap.set("n", "<localleader>ptdf", "<cmd>Python test_file_debug<cr>", { desc = "python.nvim: run test file in debug mode." })
+        -- Test Actions
+        vim.keymap.set("n", "<localleader>tt", "<cmd>Python test<cr>", { desc = "python.nvim: python run test suite", buffer = event.buf })
+        vim.keymap.set("n", "<localleader>tm", "<cmd>Python test_method<cr>", { desc = "python.nvim: python run test method", buffer = event.buf })
+        vim.keymap.set("n", "<localleader>tf", "<cmd>Python test_file<cr>", { desc = "python.nvim: python run test file", buffer = event.buf })
+        vim.keymap.set("n", "<localleader>tdd", "<cmd>Python test_debug<cr>", { desc = "python.nvim: run test suite in debug mode.", buffer = event.buf })
+        vim.keymap.set("n", "<localleader>tdm", "<cmd>Python test_method_debug<cr>", { desc = "python.nvim: run test method in debug mode.", buffer = event.buf })
+        vim.keymap.set("n", "<localleader>tdf", "<cmd>Python test_file_debug<cr>", { desc = "python.nvim: run test file in debug mode.", buffer = event.buf })
 
--- VEnv Actions
-vim.keymap.set("n", "<localleader>ped", "<cmd>Python venv delete_select<cr>", { desc = "python.nvim: select and delete a known venv." })
-vim.keymap.set("n", "<localleader>peD", "<cmd>Python venv delet<cr>", { desc = "python.nvim: delete current venv set." })
+        -- VEnv Actions
+        vim.keymap.set("n", "<localleader>ed", "<cmd>Python venv delete_select<cr>", { desc = "python.nvim: select and delete a known venv.", buffer = event.buf })
+        vim.keymap.set("n", "<localleader>eD", "<cmd>Python venv delet<cr>", { desc = "python.nvim: delete current venv set.", buffer = event.buf })
 
--- Language Actions
-vim.keymap.set("n", "<localleader>ppe", "<cmd>Python treesitter toggle_enumerate<cr>", { desc = "python.nvim: turn list into enumerate" })
-vim.keymap.set("n", "<localleader>pw", "<cmd>Python treesitter wrap_cursor<cr>", { desc = "python.nvim: wrap treesitter identifier with pattern" })
+        -- Language Actions
+        vim.keymap.set("n", "<localleader>pe", "<cmd>Python treesitter toggle_enumerate<cr>", { desc = "python.nvim: turn list into enumerate", buffer = event.buf })
+        vim.keymap.set("n", "<localleader>w", "<cmd>Python treesitter wrap_cursor<cr>", { desc = "python.nvim: wrap treesitter identifier with pattern", buffer = event.buf })
 
-require("which-key").add({
-    { "<localleader>p", group = "Python" },
-    { "<localleader>pe", group = "Python Env" },
-    { "<localleader>pp", group = "Python Lang" },
-    { "<localleader>pt", group = "Python Test" },
+        require("which-key").add({
+            { "<localleader>", group = "Python", buffer = event.buf },
+            { "<localleader>e", group = "Python Env", buffer = event.buf },
+            { "<localleader>p", group = "Python Lang", buffer = event.buf },
+            { "<localleader>t", group = "Python Test", buffer = event.buf },
+        })
+    end,
 })
