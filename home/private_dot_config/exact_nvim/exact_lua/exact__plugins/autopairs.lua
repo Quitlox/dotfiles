94a5f552a8bef42faa0f9a75e83247901447ead3
Kevin Witlox <kevin.witlox@tno.nl>
2025-11-20 22:05:01 +0100

    feat(nvim): add more autopair rules

--- home/private_dot_config/exact_nvim/exact_lua/exact__plugins/autopairs.lua
+++ home/private_dot_config/exact_nvim/exact_lua/exact__plugins/autopairs.lua
@@ -38,6 +38,98 @@ end
 local quote = quote_creator(npairs.config)
 
 -- stylua: ignore start
+-- +---------------------------+
+-- | General Rules             |
+-- +---------------------------+
+-- Auto-center upon opening brackets
+npairs.get_rule("{"):replace_map_cr(function()
+    local res = "<c-g>u<CR><CMD>normal! ====<CR><up><end><CR>"
+    local line = vim.fn.winline()
+    local height = vim.api.nvim_win_get_height(0)
+    -- Check if current line is within [1/3, 2/3] of the screen height.
+    -- If not, center the current line.
+    if line < height / 3 or height * 2 / 3 < line then
+        -- Here, 'x' is a placeholder to make sure the indentation doesn't break.
+        res = res .. "x<ESC>zzs"
+    end
+    return res
+end)
+
+-- Move past punctuation
+for _, punct in pairs({ ",", ";" }) do
+    npairs.add_rules({
+        Rule("", punct)
+            :with_move(function(opts) return opts.char == punct end)
+            :with_pair(function() return false end)
+            :with_del(function() return false end)
+            :with_cr(function() return false end)
+            :use_key(punct),
+    })
+end
+
+-- Mirror spaces inside brackets
+local brackets = { { "(", ")" }, { "[", "]" }, { "{", "}" } }
+npairs.add_rules({
+    -- Rule for a pair with left-side ' ' and right side ' '
+    Rule(" ", " ")
+        -- Pair will only occur if the conditional function returns true
+        :with_pair(function(opts)
+            -- We are checking if we are inserting a space in (), [], or {}
+            local pair = opts.line:sub(opts.col - 1, opts.col)
+            return vim.tbl_contains({
+                brackets[1][1] .. brackets[1][2],
+                brackets[2][1] .. brackets[2][2],
+                brackets[3][1] .. brackets[3][2],
+            }, pair)
+        end)
+        :with_move(cond.none())
+        :with_cr(cond.none())
+        -- We only want to delete the pair of spaces when the cursor is as such: ( | )
+        :with_del(function(opts)
+            local col = vim.api.nvim_win_get_cursor(0)[2]
+            local context = opts.line:sub(col - 1, col + 2)
+            return vim.tbl_contains({
+                brackets[1][1] .. "  " .. brackets[1][2],
+                brackets[2][1] .. "  " .. brackets[2][2],
+                brackets[3][1] .. "  " .. brackets[3][2],
+            }, context)
+        end),
+})
+
+-- For each pair of brackets we will add another rule
+for _, bracket in pairs(brackets) do
+    npairs.add_rules({
+        -- Each of these rules is for a pair with left-side '( ' and right-side ' )' for each bracket type
+
+        Rule(bracket[1] .. " ", " " .. bracket[2])
+            :with_pair(cond.none())
+            :with_move(function(opts) return opts.char == bracket[2] end)
+            :with_del(cond.none())
+            :use_key(bracket[2])
+            -- Removes the trailing whitespace that can occur without this
+            :replace_map_cr(function(_) return "<C-c>2xi<CR><C-c>O" end),
+    })
+end
+
+-- +---------------------------+
+-- | Javascript Rules          |
+-- +---------------------------+
+-- Open arrow function (=> {})
+npairs.add_rule(
+    Rule('%(.*%)%s*%=>$', ' {  }', { 'typescript', 'typescriptreact', 'javascript' })
+        :use_regex(true) 
+        :set_end_pair_length(2)
+)
+
+-- +---------------------------+
+-- | Lua Rules                 |
+-- +---------------------------+
+-- Add trailing commas in lua
+npairs.add_rules({
+    Rule("{", "},", "lua"):with_pair(ts_conds.is_ts_node({ "table_constructor" })),
+    Rule("'", "',", "lua"):with_pair(ts_conds.is_ts_node({ "table_constructor" })),
+    Rule('"', '",', "lua"):with_pair(ts_conds.is_ts_node({ "table_constructor" })),
+})
 
 -- +---------------------------+
 -- | Rust Rules                |
