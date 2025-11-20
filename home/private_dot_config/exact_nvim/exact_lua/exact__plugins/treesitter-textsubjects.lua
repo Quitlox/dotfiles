f567d105877357607527a10c2048fe3481303152
Kevin Witlox <kevin.witlox@tno.nl>
2025-11-20 22:05:34 +0100

    fix(nvim): update config textsubjects for incremental selection

--- home/private_dot_config/exact_nvim/exact_lua/exact__plugins/treesitter-textsubjects.lua
+++ home/private_dot_config/exact_nvim/exact_lua/exact__plugins/treesitter-textsubjects.lua
@@ -3,21 +3,20 @@
 -- | Selection                                               |
 -- +---------------------------------------------------------+
 
----@diagnostic disable-next-line: missing-fields
-require("nvim-treesitter.configs").setup({
-    textsubjects = {
-        enable = true,
-        prev_selection = ",",
-        keymaps = {
-            ["."] = "textsubjects-smart",
-            -- [";"] = "textsubjects-container-outer", -- FIXME: conflict with flash
-            -- ["i;"] = "textsubjects-container-inner",
-        },
+require("nvim-treesitter-textsubjects").configure({
+    -- prev_selection = ",",
+    prev_selection = "<bs>",
+    keymaps = {
+        -- ["."] = "textsubjects-smart",
+        ["<cr>"] = "textsubjects-smart",
+        -- [";"] = "textsubjects-container-outer", -- FIXME: conflict with flash
+        -- ["i;"] = "textsubjects-container-inner",
     },
 })
 
 require("which-key").add({
-    { ".", desc = "Smart Select", mode = "v" },
+    -- { ".", desc = "Smart Select", mode = "v" },
+    { "<cr>", desc = "Smart Select", mode = "v" },
     -- { ";", desc = "Select outside containers", mode = "v" },
     -- { "i;", desc = "Select inside containers", mode = "v" },
 })
