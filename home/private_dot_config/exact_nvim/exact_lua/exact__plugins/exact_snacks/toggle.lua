f2c177024488619d1b83f32337eb6ab52a3f5585
Kevin Witlox <kevin.witlox@upcmail.nl>
2025-12-01 21:21:25 +0100

    chore(nvim): reorder toggles

--- home/private_dot_config/exact_nvim/exact_lua/exact__plugins/exact_snacks/toggle.lua
+++ home/private_dot_config/exact_nvim/exact_lua/exact__plugins/exact_snacks/toggle.lua
@@ -5,7 +5,9 @@
 Snacks.toggle.diagnostics():map("yod")
 Snacks.toggle.indent():map("yoi")
 Snacks.toggle.inlay_hints():map("yoh")
-Snacks.toggle.option("relativenumber"):map("yor")
 Snacks.toggle.treesitter():map("yot")
 Snacks.toggle.zen():map("yoz")
 Snacks.toggle.zoom():map("yom")
+
+Snacks.toggle.option("relativenumber"):map("yor")
+Snacks.toggle.option("wrap"):map("yow")
