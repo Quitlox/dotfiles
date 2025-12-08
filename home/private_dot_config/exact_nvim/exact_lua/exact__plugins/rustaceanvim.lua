0877345c4aeb0eda9fa03c6e3dcde8b6e0aa46b6
Kevin Witlox <kevin.witlox@tno.nl>
2025-12-08 20:16:22 +0100

    fix(nvim): properly configure rustanalyzer, disable snippets

--- home/private_dot_config/exact_nvim/exact_lua/exact__plugins/rustaceanvim.lua
+++ home/private_dot_config/exact_nvim/exact_lua/exact__plugins/rustaceanvim.lua
@@ -40,22 +40,24 @@ vim.api.nvim_create_autocmd("FileType", {
 
 vim.g.rustaceanvim = {
     server = {
-        default_settings = {
-            ["rust-analyzer"] = {},
-        },
-    },
+        -- I want VS Code settings to override my settings,
+        -- not the other way around, so use codesettings.nvim
+        -- instead of rustaceanvim's built-in vscode settings loader
+        load_vscode_settings = false,
+        -- the global hook doesn't work when configuring rust-analyzer with rustaceanvim
+        settings = function(_, config)
+            return require("codesettings").with_local_settings("rust-analyzer", config)
+        end,
 
-    -- I want VS Code settings to override my settings,
-    -- not the other way around, so use codesettings.nvim
-    -- instead of rustaceanvim's built-in vscode settings loader
-    load_vscode_settings = false,
-    -- the global hook doesn't work when configuring rust-analyzer with rustaceanvim
-    settings = function(_, config)
-        return require("codesettings").with_local_settings("rust-analyzer", config)
-    end,
-    default_settings = {
-        ["rust-analyzer"] = {
-            -- your global LSP settings go here
+        default_settings = {
+            ["rust-analyzer"] = {
+                completion = {
+                    callable = {
+                        -- Do not add arguments when completing (interferes with autopairs I think)
+                        snippets = "none",
+                    },
+                },
+            },
         },
     },
 }
