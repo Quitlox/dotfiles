85d204e6fdeffdb2b50f3a72b81a7b5440d08479
Kevin Witlox <kevin.witlox@upcmail.nl>
2025-12-19 10:10:40 +0100

    chore(nvim): update plugins

--- home/private_dot_config/exact_nvim/exact_lua/exact__plugins/treesitter-textsubjects.lua
+++ home/private_dot_config/exact_nvim/exact_lua/exact__plugins/treesitter-textsubjects.lua
@@ -3,20 +3,20 @@
 -- | Selection                                               |
 -- +---------------------------------------------------------+
 
--- require("nvim-treesitter-textsubjects").configure({
---     -- prev_selection = ",",
---     prev_selection = "<bs>",
---     keymaps = {
---         -- ["."] = "textsubjects-smart",
---         ["<cr>"] = "textsubjects-smart",
---         -- [";"] = "textsubjects-container-outer", -- FIXME: conflict with flash
---         -- ["i;"] = "textsubjects-container-inner",
---     },
--- })
---
--- require("which-key").add({
---     -- { ".", desc = "Smart Select", mode = "v" },
---     { "<cr>", desc = "Smart Select", mode = "v" },
---     -- { ";", desc = "Select outside containers", mode = "v" },
---     -- { "i;", desc = "Select inside containers", mode = "v" },
--- })
+require("nvim-treesitter-textsubjects").configure({
+    -- prev_selection = ",",
+    prev_selection = "<bs>",
+    keymaps = {
+        -- ["."] = "textsubjects-smart",
+        ["<cr>"] = "textsubjects-smart",
+        -- [";"] = "textsubjects-container-outer", -- FIXME: conflict with flash
+        -- ["i;"] = "textsubjects-container-inner",
+    },
+})
+
+require("which-key").add({
+    -- { ".", desc = "Smart Select", mode = "v" },
+    { "<cr>", desc = "Smart Select", mode = "v" },
+    -- { ";", desc = "Select outside containers", mode = "v" },
+    -- { "i;", desc = "Select inside containers", mode = "v" },
+})
