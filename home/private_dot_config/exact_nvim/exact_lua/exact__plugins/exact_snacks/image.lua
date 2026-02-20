04f50043e54d44cf7dc7c45ac24f1e7a33e16289
Kevin Witlox <kevin.witlox@upcmail.nl>
2026-02-20 16:07:48 +0100

    fix(nvim): disable image popup on latex

--- home/private_dot_config/exact_nvim/exact_lua/exact__plugins/exact_snacks/image.lua
+++ home/private_dot_config/exact_nvim/exact_lua/exact__plugins/exact_snacks/image.lua
@@ -5,7 +5,7 @@
 local image_opts = {
     enabled = vim.fn.has("wsl") ~= 1,
     doc = { inline = false, float = true, conceal = true },
-    math = { enabled = true, latex = { font_size = "Large" } },
+    math = { enabled = false, latex = { font_size = "Large" } },
 }
 
 return image_opts
