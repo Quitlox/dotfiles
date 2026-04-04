4ac52871446c6f46bcbca4e8d58862ebac7cd020
Kevin Witlox <kevin.witlox@upcmail.nl>
2026-04-04 23:17:12 +0200

    fix(nvim): add global codesettings.json

--- home/private_dot_config/exact_nvim/exact_lua/exact__plugins/codesettings.lua
+++ home/private_dot_config/exact_nvim/exact_lua/exact__plugins/codesettings.lua
@@ -7,6 +7,9 @@ codesettings.setup({})
 
 vim.lsp.config("*", {
     before_init = function(_, config)
-        config = codesettings.with_local_settings(config.name, config)
+        -- Load global defaults from Neovim config dir first
+        codesettings.loader():root_dir(vim.fn.stdpath("config")):with_local_settings(config.name, config)
+        -- Load project-local settings second (overrides global)
+        codesettings.loader():root_dir(config.root_dir):with_local_settings(config.name, config)
     end,
 })
