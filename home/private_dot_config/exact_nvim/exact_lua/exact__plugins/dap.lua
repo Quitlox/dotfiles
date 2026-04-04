cd661f2d6532d75b5c9578a8c96828d69735008d
Kevin Witlox <kevin.witlox@upcmail.nl>
2026-04-04 23:17:32 +0200

    fix(nvim): cleanup dap.lua

--- home/private_dot_config/exact_nvim/exact_lua/exact__plugins/dap.lua
+++ home/private_dot_config/exact_nvim/exact_lua/exact__plugins/dap.lua
@@ -7,28 +7,6 @@ local dap = require("dap")
 -- dap.defaults.fallback
 dap.defaults.fallback.switchbuf = "useopen,uselast"
 
---+- Launch.json --------------------------------------------+
--- Command: Reload launch.json
-local function load_launch_json()
-    local status, err = pcall(require("dap.ext.vscode").load_launchjs)
-    if not status then
-        require("notify")("Is there a typo in the config?\n\n" .. err, "ERROR", { title = "Error while loading .vscode/launch.json" })
-    end
-end
-
--- Load launch.json when edited
-local launch_group = vim.api.nvim_create_augroup("LaunchJson", { clear = true })
-vim.api.nvim_create_autocmd("BufWritePost", {
-    group = launch_group,
-    desc = "Reload launch.json on save",
-    pattern = "launch.json",
-    callback = load_launch_json,
-})
-
-vim.api.nvim_create_user_command("LoadLaunchJson", load_launch_json, {
-    desc = "Reload launch.json",
-})
-
 --+- Keymaps ------------------------------------------------+
 -- stylua: ignore start
 vim.keymap.set("n", "<F9>", function() require("dap").continue() end, { noremap = true, silent = true, desc = "Debug Continue" })
@@ -46,6 +24,12 @@ vim.keymap.set("n", "<leader>dbm", function() require("dap").set_breakpoint(nil,
 vim.keymap.set("n", "<leader>dbc", function() require("dap").toggle_breakpoint(vim.fn.input("Condition: ")) end, { noremap = true, silent = true, desc = "Breakpoint Condition" })
 
 vim.keymap.set("n", "<leader>dv", "<cmd>LoadLaunchJson<cr>", { noremap = true, silent = true, desc = "Reload launch.json" })
+-- stylua: ignore end
+
+--+- Keymaps: nvim-dap-view ---------------------------------+
+-- stylua: ignore start
+vim.keymap.set("n", "<localleader>do", "<cmd>DapViewOpen<cr>", { noremap = true, silent = true, desc = "DapViewOpen" })
+vim.keymap.set("n", "<localleader>dc", "<cmd>DapViewClose<cr>", { noremap = true, silent = true, desc = "DapViewClose" })
 -- stylua: ignore end
 
 --+- DAP: Javascript / Typescript ---------------------------+
@@ -103,17 +87,19 @@ function PromptBackspace()
     end
 end
 
-vim.cmd([[
-fun! PromptBackspaceSetup()
-    if v:option_new == 'prompt'
-      " Only apply if the filetype is not 'codecompanion'
-      if getbufvar(bufnr(), '&filetype') !=# 'codecompanion'
-        silent! inoremap <buffer><silent> <backspace> <Cmd>lua PromptBackspace()<cr>
-        " Fix <C-BS> in prompt
-        silent! inoremap <buffer><silent> <C-BS> <Cmd>normal! bcw<cr>
-      endif
-    endif
-endfun
+-- NOTE: Should no longer be necessary?
 
-silent! autocmd OptionSet * call PromptBackspaceSetup() "not using a lua function here because of error when accessing :help
-]])
+-- vim.cmd([[
+-- fun! PromptBackspaceSetup()
+--     if v:option_new == 'prompt'
+--       " Only apply if the filetype is not 'codecompanion'
+--       if getbufvar(bufnr(), '&filetype') !=# 'codecompanion'
+--         silent! inoremap <buffer><silent> <backspace> <Cmd>lua PromptBackspace()<cr>
+--         " Fix <C-BS> in prompt
+--         silent! inoremap <buffer><silent> <C-BS> <Cmd>normal! bcw<cr>
+--       endif
+--     endif
+-- endfun
+--
+-- silent! autocmd OptionSet * call PromptBackspaceSetup() "not using a lua function here because of error when accessing :help
+-- ]])
