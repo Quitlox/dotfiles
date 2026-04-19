913a8b5eb6f625fb54fe0a24eb5f0549d838f335
Kevin Witlox <kevin.witlox@upcmail.nl>
2026-04-19 16:43:11 +0200

    fix(nvim): deprecation warning

--- home/private_dot_config/exact_nvim/exact_lua/exact__plugins/lspconfig.lua
+++ home/private_dot_config/exact_nvim/exact_lua/exact__plugins/lspconfig.lua
@@ -20,7 +20,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
             return
         end
 
-        if (client.supports_method and client.supports_method("textDocument/inlayHint")) or (client.server_capabilities and client.server_capabilities.inlayHintProvider) then
+        if (client.supports_method and client:supports_method("textDocument/inlayHint")) or (client.server_capabilities and client.server_capabilities.inlayHintProvider) then
             -- vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
         end
     end,
