b5b3ede63257e1762b4305ca5c545420a5f17be2
Kevin Witlox <kevin.witlox@upcmail.nl>
2026-04-23 10:55:24 +0200

    feat(zsh): warn if wsl alias broken

--- home/private_dot_config/zsh/wsl.zsh
+++ home/private_dot_config/zsh/wsl.zsh
@@ -15,5 +15,37 @@ done
 export PATH="$NEW_PATH"
 
 # Create aliases for Windows executables with direct paths
-alias explorer.exe="/mnt/c/Windows/explorer.exe"
-alias sioyek.exe="/mnt/c/Users/witloxkhd/Applications/sioyek/sioyek.exe"
+typeset -A _wsl_aliases=(
+  [explorer.exe]="/mnt/c/Windows/explorer.exe"
+  [sioyek.exe]="/mnt/c/Users/witloxkhd/AppData/Local/Microsoft/WinGet/Packages/ahrm.sioyek_Microsoft.Winget.Source_8wekyb3d8bbwe/sioyek-release-windows/sioyek.exe"
+  [win32yank.exe]="/mnt/c/Program Files/Neovim/bin/win32yank.exe"
+)
+
+# ------------------------------------------------------------------------------
+# Set aliases, print warnings for broken links
+# ------------------------------------------------------------------------------
+_wsl_warnings=()
+for _name _target in "${(@kv)_wsl_aliases}"; do
+  if [[ -x "$_target" ]]; then
+    alias "$_name=$_target"
+  else
+    _wsl_warnings+=("[warn] WSL alias '$_name' target not found: $_target")
+  fi
+done
+unset _name _target _wsl_aliases
+
+# Defer warnings until after the prompt is drawn (once)
+if (( ${#_wsl_warnings} )); then
+  function _wsl_print_warnings() {
+    for _msg in "${_wsl_warnings[@]}"; do
+      print -u2 "$_msg"
+    done
+    print -u2 "       Edit \${XDG_CONFIG_HOME:-~/.config}/zsh/wsl.zsh to fix."
+    unset _wsl_warnings
+    precmd_functions=(${precmd_functions:#_wsl_print_warnings})
+    unfunction _wsl_print_warnings
+  }
+  precmd_functions+=(_wsl_print_warnings)
+else
+  unset _wsl_warnings
+fi
