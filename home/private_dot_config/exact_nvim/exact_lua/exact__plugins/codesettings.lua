3fedb5e556e209f8102562a100faba1728acce1e
Kevin Witlox <kevin.witlox@upcmail.nl>
2026-05-27 16:06:02 +0200

    fix(nvim): bug in codesttings.lua

--- home/private_dot_config/exact_nvim/exact_lua/exact__plugins/codesettings.lua
+++ home/private_dot_config/exact_nvim/exact_lua/exact__plugins/codesettings.lua
@@ -10,6 +10,8 @@ vim.lsp.config("*", {
         -- Load global defaults from Neovim config dir first
         codesettings.loader():root_dir(vim.fn.stdpath("config")):with_local_settings(config.name, config)
         -- Load project-local settings second (overrides global)
-        codesettings.loader():root_dir(config.root_dir):with_local_settings(config.name, config)
+        if config.root_dir then
+            codesettings.loader():root_dir(config.root_dir):with_local_settings(config.name, config)
+        end
     end,
 })
