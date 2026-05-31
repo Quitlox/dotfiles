5b25639b2b869559f61d977032a4530377195a9e
Kevin Witlox <kevin.witlox@upcmail.nl>
2026-05-31 14:28:48 +0200

    feat(nix): make containers log to journal

--- nixos/modules/extra-arion.nix
+++ nixos/modules/extra-arion.nix
@@ -7,4 +7,11 @@
   environment.systemPackages = [ pkgs.arion ];
   virtualisation.docker.enable = true;
   virtualisation.arion.backend = "docker";
+
+  # Default all containers to log to the systemd journal instead of json-file.
+  # View with e.g. `journalctl CONTAINER_NAME=profilarr` or `journalctl -t profilarr`.
+  virtualisation.docker.daemon.settings = {
+    log-driver = "journald";
+    log-opts.tag = "{{.Name}}";
+  };
 }
