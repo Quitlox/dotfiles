701358f1dad0fbc9efe04faba914bb8d13a7a9e5
Kevin Witlox <kevin.witlox@upcmail.nl>
2026-05-31 15:22:01 +0200

    feat(nix): add dashy

--- nixos/hosts/quitlox-homelab/configuration.nix
+++ nixos/hosts/quitlox-homelab/configuration.nix
@@ -23,6 +23,7 @@
     ../../modules/service-traefik.nix
     ./modules/service-ddns-updater.nix
     ./modules/service-media.nix
+    ./modules/application-dashy.nix
     # server defaults (nice-to-haves) from srvos
     (srvos.outPath + "/nixos/common/detect-hostname-change.nix")
     (srvos.outPath + "/nixos/common/update-diff.nix")
---
+++ nixos/hosts/quitlox-homelab/modules/application-dashy.nix
@@ -0,0 +1,52 @@
+# Dashy - The Ultimate Homepage for your Homelab
+#
+# Deployment
+# - dashy.home.quitlox.dev
+{ config, ... }:
+let
+  domain = config.quitlox.traefik.domain;
+
+  # Unpriviliged user for the container and the configuration file.
+  dashyUID = 1501;
+  dashyGID = 1501;
+in
+{
+  systemd.tmpfiles.rules = [
+    "d /var/lib/dashy/ 0700 dashy dashy - -"
+    "f /var/lib/dashy/conf.yml 0644 dashy dashy - -"
+  ];
+
+  # Unpriviliged user for the container and the configuration file.
+  users.groups.dashy.gid = dashyGID;
+  users.users.dashy = {
+    uid = dashyUID;
+    home = "/var/lib/dashy";
+    group = "dashy";
+    isSystemUser = true;
+  };
+
+  virtualisation.arion.projects.dashy.settings = {
+    project.name = "dashy";
+    networks.proxy.name = "proxy";
+    services.dashy.service = {
+      image = "lissy93/dashy";
+      container_name = "dashy";
+      restart = "unless-stopped";
+      user = "${toString dashyUID}:${toString dashyGID}";
+      networks = [ "proxy" ];
+      environment = {
+        TZ = "Europe/Amsterdam";
+      };
+      volumes = [
+        "/var/lib/dashy:/app/user-data"
+      ];
+      labels = {
+        "traefik.enable" = "true";
+        "traefik.http.routers.dashy.rule" = "Host(`dashy.${domain}`)";
+        "traefik.http.routers.dashy.middlewares" = "ip-internal@file";
+        "traefik.http.routers.dashy.service" = "dashy";
+        "traefik.http.services.dashy.loadbalancer.server.port" = "8080";
+      };
+    };
+  };
+}
