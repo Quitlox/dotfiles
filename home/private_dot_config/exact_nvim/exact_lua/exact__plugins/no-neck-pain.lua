59430272d0f25feec6f2d0b4c0d493eed0d2028b
Kevin Witlox <kevin.witlox@upcmail.nl>
2026-07-01 09:17:58 +0200

    fix(nvim): no-neck-pain and edgy integration

--- home/private_dot_config/exact_nvim/exact_lua/exact__plugins/no-neck-pain.lua
+++ home/private_dot_config/exact_nvim/exact_lua/exact__plugins/no-neck-pain.lua
@@ -2,11 +2,44 @@
 -- | shortcuts/no-neck-pain.nvim                             |
 -- +---------------------------------------------------------+
 
+local constants = require("no-neck-pain.util.constants")
+
+local edgy_integrations = {
+    GrugFar = { fileTypePattern = "grug-far", position = "right" },
+    CopilotChat = { fileTypePattern = "copilot-chat", position = "right" },
+    CodeCompanion = { fileTypePattern = "codecompanion", position = "right" },
+    Sidekick = { fileTypePattern = "sidekick", position = "right" },
+    SnacksTerminal = { fileTypePattern = "snacks_terminal", position = "none" },
+    Overseer = { fileTypePattern = "overseer", position = "none" },
+    Neogit = { fileTypePattern = "neogit", position = "none" },
+    Gitlab = { fileTypePattern = "gitlab", position = "none" },
+    Trouble = { fileTypePattern = "trouble", position = "none" },
+    QuickFix = { fileTypePattern = "qf", position = "none" },
+    Help = { fileTypePattern = "help", position = "none" },
+    Spectre = { fileTypePattern = "spectre", position = "none" },
+    QuickTest = { fileTypePattern = "quicktest", position = "none" },
+    OutputPanel = { fileTypePattern = "outputpanel", position = "none" },
+}
+
+for name, spec in pairs(edgy_integrations) do
+    constants.INTEGRATIONS[name] = {
+        fileTypePattern = spec.fileTypePattern,
+        close = "",
+        open = "",
+    }
+end
+
+local integrations_config = {}
+for name, spec in pairs(edgy_integrations) do
+    integrations_config[name] = { position = spec.position, reopen = false }
+end
+
 require("no-neck-pain").setup({
     width = 120,
     autocmds = {
         skipEnteringNoNeckPainBuffer = true,
     },
+    integrations = integrations_config,
 })
 
 vim.g.toggle_no_neck_pain = false
